6f4e36b6639d4e65c7e17b9d371a7f950f679dee
Aayush Singh <aayushkt@gmail.com>
2023-08-24 13:04:32 -0400

    turn of aos on small screens

--- public/main.js
+++ public/main.js
@@ -5,3 +5,12 @@ function scrollToCard(cardId) {
         inline: 'center'
     });
 }
+
+// Turn off AOS on smaller screens
+if (window.innerWidth < 800){
+    duration = { duration: 0};
+} else {
+    duration = { duration: 1000};
+}
+
+AOS.init(duration);
